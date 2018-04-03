7eab136398a303998e5bcbf477c0f11f94b24fab
Nate Mills <nathan.mills@avibeweb.com>
2018-04-03 09:01:09 -0700

    test Message

--- Commands/gitCommit.py
+++ Commands/gitCommit.py
@@ -5,19 +5,16 @@ from ..Core.Controller import *
 
 class gitCommitCommand(sublime_plugin.TextCommand, gitController):
 	def run(self, edit):
-		print("Commit!! jk")
 		self.path = self.get_git_dir()
 		if len(self.path) == 0:
 			return;
 
 
 		self.path = self.get_scoped_path(git_settings().get('commit_scope', 'file'))
-		self.dir = self.get_scoped_path('repo')
+		self.repoDir = self.get_scoped_path('repo')
 
 		print(self.path)
-		print(self.dir)
+		print(self.repoDir)
 
 		message = "test Message"
-		self.run_git_command(["git", "commit", "-m", message, self.dir], self.dir)
-
-		print("Commit!! jk")
+		self.run_git_command(["git", "commit", "-m", message, self.path], self.repoDir)

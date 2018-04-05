a6fa52499e9d5e806d40ed299de4a59850ce1f4d
Nate Mills <mills.nate@gmail.com>
2018-04-05 10:17:53 -0700

    #: Testing push function

--- Commands/gitPush.py
+++ Commands/gitPush.py
@@ -6,3 +6,6 @@ from ..Core.Controller import *
 class gitPushCommand(sublime_plugin.TextCommand, gitController):
 	def run(self, edit):
 		print('push')
+		# git.exe push --progress "origin" HomeCheckOut:NateHomeCheckout
+		self.dir = self.get_scoped_path('repo')
+		self.run_git_command(["git", "push", "origin", "newTestBranch"], self.dir)

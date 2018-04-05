77b1fe44781063fd34b887b88626d66f558c378e
Nate Mills <mills.nate@gmail.com>
2018-04-05 10:27:02 -0700

    #0: Push test 2

--- Commands/gitPush.py
+++ Commands/gitPush.py
@@ -8,4 +8,16 @@ class gitPushCommand(sublime_plugin.TextCommand, gitController):
 		print('push')
 		# git.exe push --progress "origin" HomeCheckOut:NateHomeCheckout
 		self.dir = self.get_scoped_path('repo')
-		self.run_git_command(["git", "push", "origin"], self.dir)
+		branch = git_settings().get('remoteBranch', '')
+		if len(branch) == 0:
+			self.set_branch()
+		else:
+			self.do_push(branch)
+
+	def set_branch():
+		sublime.active_window().show_input_panel("Remote Brach Name:", "", self.do_push, None, None)
+
+	def do_push(self, remoteBranch):
+		git_settings().set('history', history)
+		sublime.save_settings('GitTools.sublime-settings')
+		self.run_git_command(["git", "push", "origin", remoteBranch], self.dir)
